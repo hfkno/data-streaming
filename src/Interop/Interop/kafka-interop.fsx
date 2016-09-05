8471ee86c9b242e86d652b1c68a2a517df756c3a
Aaron Comyn <aaron@roxor.no>
2016-09-05 11:13:41 +0200

    Making the schema register more usable

--- src/Interop/Interop/kafka-interop.fsx
+++ src/Interop/Interop/kafka-interop.fsx
@@ -41,19 +41,30 @@ Http.RequestString
 
 
 *)
-
+type Result<'TSuccess,'TError> = 
+     | Success of 'TSuccess 
+     | Error of 'TError
 let (|Exists|_|) (str:string) = if System.String.IsNullOrEmpty str then None else Some str
 let cleanJson (json:string) = json.Replace("\\\"", "\"")
-let splitty (str:string) =
-    match str with
-    | Exists s -> str.Replace("[", "").Replace("]", "").Replace("\"", "").Split(',')
-    | _ -> [||]
+let splitJsonArray (input:Result<string,string>) =
+    match input with
+    | Success str ->
+        match str with
+        | Exists s -> str.Replace("[", "").Replace("]", "").Replace("\"", "").Split(',')
+        | _ -> [||]
+    | Error msg -> [| msg |]
+
 
 
 type ConfluenceAdapter(rootUrl) =
     member x.url ending = rootUrl + "/" + ending
-    member x.request(url, ?headers, ?body) = 
-        Http.RequestString(url, ?headers = headers, ?body = body) |> cleanJson
+    member x.request(url, ?headers, ?body) : Result<string,string> = 
+        try 
+            Http.RequestString(url, ?headers = headers, ?body = body) 
+            |> cleanJson 
+            |> Success
+        with 
+            | :? System.Net.WebException as ex -> Error ex.Message
     member x.list(topic)  = x.request (x.url topic)
 
 
@@ -61,6 +72,7 @@ type ConfluenceAdapter(rootUrl) =
 type Kafka(rootUrl) =
     inherit ConfluenceAdapter(rootUrl)
     member x.listTopics()  = x.request (x.url "topics")
+    member x.topics() = x.listTopics() |> splitJsonArray
     member x.listSchemas() = x.request (x.url "topics/_schemas")
     member x.produceMessage(topic, msg) = 
         x.request 
@@ -74,16 +86,22 @@ let k = new Kafka("http://localhost:8082")
 k.list("topics/")
 k.listTopics()
 k.listSchemas()
+k.topics()
 
 // produding a message with Avro metadata embedded
 k.produceMessage("basictest2", """{"value_schema": "{\"type\": \"record\", \"name\": \"User\", \"fields\": [{\"name\": \"name\", \"type\": \"string\"}]}", "records": [{"value": {"name": "testUser"}}]}""")
 
 
 
+
+
+
 (* Schema manipulation  *)
 type Registry(rootUrl) =
     inherit ConfluenceAdapter(rootUrl)
-    member x.listTopics()  = x.request (x.url "schemas/ids/0")
+    member x.listTopics()  = x.request (x.url "subjects")
+    member x.topics() = x.listTopics() |> splitJsonArray
+    member x.schema(id:int) = x.request (x.url (sprintf "schemas/ids/%i" id))
 
 
 let r = new Registry("http://localhost:8081")
@@ -93,10 +111,6 @@ r.list("schemas/ids/1")
 r.list("subjects")
 r.list("subjects/basictest2-value/versions")
 r.list("subjects/basictest-value/versions/1")
-
-
-
-
-splitty null
-splitty ""
-splitty """["__consumer_offsets","_schemas","avrotest","basictest","basictest2","test"]"""
+r.listTopics()
+r.topics()
+r.schema(1)
