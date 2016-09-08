c8bd7411d0947b260beb06c34c07ef6411e6fdb8
Aaron Winston Comyn <aaron.winston.comyn@hfk.no>
2016-09-08 14:48:54 +0200

    Akka messaging

--- src/Interop/Interop/akka_basics.fsx
+++ src/Interop/Interop/akka_basics.fsx
@@ -3,7 +3,7 @@
 
 (*
 
-    Basic Akka.Net functionality
+    Akka.Net Fundamentals
 
 *)
 
@@ -15,27 +15,62 @@
 #r "../../packages/System.Collections.Immutable/lib/portable-net45+win8+wp8+wpa81/System.Collections.Immutable.dll"
 #r "../../packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
 
-
 open Akka
 open Akka.Actor
+open Akka.Configuration
 open Akka.FSharp
 
+
+
+(* 
+
+    Basic messaging functionality 
+
+*)
 type Greet(who) = 
     member x.Who = who
 
 type GreetingActor() as g =
     inherit ReceiveActor()
-    do g.Receive<Greet>(fun (greet:Greet) -> printfn "\r\nGiving a greeting: %s\r\n" greet.Who)
+    do g.Receive<Greet>
+        (fun (greet:Greet) -> printfn "\r\nReceived a greeting, hello: %s\r\n" greet.Who)
+
+let system = ActorSystem.Create("greeting-system")
+let greeter = system.ActorOf<GreetingActor> "greeter-actor"
+
+"world" |> Greet |> greeter.Tell
+
+system.Terminate()
+
+
+
+(* 
+    
+    Actor coordination functionality 
+
+    An Actor is a like a thread instance with a mailbox. 
+
+*)
+
+
+type EchoServer =
+    inherit Actor
+
+    override x.OnReceive message =
+        match message with
+        | :? string as msg -> printfn "Hello %s\r\n" msg
+        | _ ->  failwith "unknown message"
+
+let sys = ActorSystem.Create("FSharp")
+let echoServer = sys.ActorOf(Props(typedefof<EchoServer>, Array.empty))
+
+echoServer <! "F#!"
+
+sys.Terminate()
+
+
 
 
 
 Configuration.defaultConfig()
 //use sys = System.create "my-greeting-system" ()
-let system = ActorSystem.Create("GreetingSystem")
-let greeter = system.ActorOf<GreetingActor> "greeter"
-
-"World" |> Greet |> greeter.Tell
-
-
-
-
