65ec0cbf1b9105904cae8bbb6ece3169061567a9
Aaron Winston Comyn <aaron.winston.comyn@hfk.no>
2016-11-17 13:35:18 +0100

    FSharp formatting

--- src/Interop/Interop/ad_vismae_integration.fsx
+++ src/Interop/Interop/ad_vismae_integration.fsx
@@ -1,1 +1,24 @@
 ﻿
+(**
+# Active Directoy -> Visma Enterprise Integration
+
+## Integration overview
+
+  * Retreive users from Visma Enterprise
+  * Retreive users from Active Directory
+  * Synch changes from active directory with Visma user data
+  * Push user changes to Visma Enterprise through their user management webservice
+*)
+
+
+(** 
+## Integration details
+Initial connection to LDAP for user retrieval
+*)
+
+(*** include: ldap-connect ***)
+
+
+(*** define: ldap-connect ***)
+let helloWorld() = printfn "Hello world!"
+
---
+++ src/Interop/Interop/make-docs.fsx
@@ -0,0 +1,30 @@
+﻿
+#load "../../packages/FSharp.Formatting/FSharp.Formatting.fsx"
+open FSharp.Literate
+open System.IO
+
+
+/// Return path relative to the current file location
+let relative subdir = Path.Combine(__SOURCE_DIRECTORY__, subdir)
+
+// Create output directories & copy content files there
+if not (Directory.Exists(relative "output")) then
+  Directory.CreateDirectory(relative "output") |> ignore
+  Directory.CreateDirectory (relative "output/content") |> ignore
+  
+// for fileInfo in DirectoryInfo(relative "../../docs/files/content").EnumerateFiles() do
+//   fileInfo.CopyTo(Path.Combine(relative "output/content", fileInfo.Name)) |> ignore
+
+
+/// Processes a single F# Script file and produce HTML output
+let processScriptAsHtml () =
+  let script = relative "ad_vismae_integration.fsx"
+  let output = relative "output/ad_visma.html"
+  let template = relative "template-file.html"
+  Literate.ProcessScriptFile(script, template, output)
+
+
+processScriptAsHtml()
+
+
+//.
