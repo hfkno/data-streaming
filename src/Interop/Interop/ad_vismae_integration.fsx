3d95063d44ebd99788c085962edeba90683a1b4e
Aaron Winston Comyn <aaron.winston.comyn@hfk.no>
2016-11-25 10:40:48 +0100

    Refactoring en route to user mnagement

--- src/Interop/Interop/ad_vismae_integration.fsx
+++ src/Interop/Interop/ad_vismae_integration.fsx
@@ -18,6 +18,20 @@ Initial connection to LDAP for user retrieval
 
 (*** include: ldap-connect ***)
 
+
+
+
+(** 
+### Implementation
+
+Active Directory is searched using the managed API
+
+*)
+
+(*** include: ad-operations ***)
+
+
+
 (*** hide ***)
 
 #r "System.DirectoryServices"
@@ -26,7 +40,80 @@ open System.DirectoryServices
 open System.DirectoryServices.AccountManagement
 
 
-    //use user = UserPrincipal.FindByIdentity(domainContext, IdentityType.Name, "A*")
+
+
+(*** define: ad-operations ***)
+
+module ActiveDirectory =
+
+    type User = {
+        Name : string
+        Account : string
+    }
+
+    let users () =
+        seq {
+            use ctx = new PrincipalContext(ContextType.Domain, "ad.hfk.no", "OU=HFK,DC=ad,DC=hfk,DC=no")
+            use qbeUser = new UserPrincipal(ctx)
+            use searcher = new PrincipalSearcher(qbeUser)
+            for u in searcher.FindAll() do
+                let ru = { Name = u.DisplayName; Account = u.SamAccountName }
+                yield ru 
+        }
+
+    // Search examples
+    //
+    //
+    //   qbeUser.GivenName <- "*"
+    //   s.QueryFilter <- (qbeUser :> Principal)
+    //   (searcher.GetUnderlyingSearcher() :?> DirectorySearcher).PageSize <- 1000
+    //   (searcher.GetUnderlyingSearcher() :?> DirectorySearcher).SizeLimit <- 0
+    //
+    //
+    //let listUsersThroughGroupSearch() =
+    //    use domainContext = new PrincipalContext(ContextType.Domain)
+    //    //use user = UserPrincipal.FindByIdentity()
+    //    use group = GroupPrincipal.FindByIdentity(domainContext, IdentityType.SamAccountName, "Domain Users")
+    //    let mutable i = 0
+    //    for u in group.GetMembers(false) do
+    //        use user = (u :?> UserPrincipal)
+    //        i <- i + 1
+    //        printfn "%i %s %s %O %O" i (user.DistinguishedName) user.SamAccountName user.UserPrincipalName user.StructuralObjectClass //(user.AccountExpirationDate.HasValue)
+    //
+    //
+    //let listAllAdGroups() =
+    //    use ctx = new PrincipalContext(ContextType.Domain, "ad.hfk.no") //, "OU=HFK");
+    //    use group = new GroupPrincipal(ctx)
+    //
+    //    use search = new PrincipalSearcher(group)
+    //    for g in search.FindAll() do
+    //        use g = (g :?> GroupPrincipal)
+    //        use user = g /// (u :?> UserPrincipal)
+    //        printfn "%s || %s" user.Name user.DistinguishedName
+    //
+    //let directLdapSearch () =
+    //    let startingPoint = new DirectoryEntry("LDAP://OU=HFK,DC=ad,DC=hfk,DC=no")
+    //    let searcher = new DirectorySearcher(startingPoint)
+    //    searcher.Filter = "(&(objectCategory=person)(objectClass=user))" |> ignore
+    //    searcher.PageSize <- 1000 //|> ignore
+    //    searcher.SizeLimit <- 0 //|> ignore
+    //
+    //    let mutable i = 0
+    //    for res in searcher.FindAll() do
+    //        i <- i + 1
+    //        printfn "%i %O" i res.Path
+
+
+(*** hide ***)
+
+let users = ActiveDirectory.users() |> Seq.toList
+users |> Seq.length
+for u in users do printfn "%A\r\n" u
+
+
+
+
+ //use user = UserPrincipal.FindByIdentity(domainContext, IdentityType.Name, "A*")
     
 //    use u = new UserPrincipal(domainContext);
 //    use s = new PrincipalSearcher(u);
@@ -43,110 +130,6 @@ open System.DirectoryServices.AccountManagement
 //
 //    for res in searcher.FindAll() do 
 //        printfn "%O" res
-
-
-
-
-
-
-//module ActiveDirectory =
-
-
-
-// We can list users... need to filter for "users" and not just "eeeeeverything" - this is through the new API and works kinda nicely
-let theActio() =
-    use domainContext = new PrincipalContext(ContextType.Domain) //, "ad.hfk.no")
-
-    use group = GroupPrincipal.FindByIdentity(domainContext, IdentityType.SamAccountName, "Domain Users")
-    let mutable i = 0
-    for u in group.GetMembers(false) do
-        use user = (u :?> UserPrincipal)
-        i <- i + 1
-        printfn "%i %s %s %O %O" i (user.DistinguishedName) user.SamAccountName user.UserPrincipalName user.StructuralObjectClass //(user.AccountExpirationDate.HasValue)
-
-theActio()
-
-
-
-// Direct search, getting an interesting number of results (3000)
-let listOrganizationalUnits () =
-    let startingPoint = new DirectoryEntry("LDAP://OU=HFK,DC=ad,DC=hfk,DC=no")
-    let searcher = new DirectorySearcher(startingPoint)
-    searcher.Filter = "(&(objectCategory=person)(objectClass=user))" |> ignore
-    searcher.PageSize <- 500 //|> ignore
-    searcher.SizeLimit <- 0 //|> ignore
-
-    let mutable i = 0
-    for res in searcher.FindAll() do
-        i <- i + 1
-        printfn "%i %O" i res.Path
-    ()
-listOrganizationalUnits()
-
-
-
-
-
-
-
-// We can list users... need to filter for "users" and not just "eeeeeverything" - this is through the new API and works kinda nicely
-let theAction() =
-    use domainContext = new PrincipalContext(ContextType.Domain) //, "ad.hfk.no")
-    //use user = UserPrincipal.FindByIdentity()
-    use group = GroupPrincipal.FindByIdentity(domainContext, IdentityType.SamAccountName, "Domain Users")
-    let mutable i = 0
-    for u in group.GetMembers(false) do
-        use user = (u :?> UserPrincipal)
-        i <- i + 1
-        printfn "%i %s %s %O %O" i (user.DistinguishedName) user.SamAccountName user.UserPrincipalName user.StructuralObjectClass //(user.AccountExpirationDate.HasValue)
-
-theAction()
-
-
-
-
-
-
-
-// This one seems to only be getting the goups, not he members...  Perhaps a user filter on the PrincipalSearcher?
-let directActiono() =
-    use ctx = new PrincipalContext(ContextType.Domain, "ad.hfk.no") //, "OU=HFK");
-    use group = new GroupPrincipal(ctx)
-
-    use search = new PrincipalSearcher(group)
-    for g in search.FindAll() do
-        use g = (g :?> GroupPrincipal)
-        use user = g /// (u :?> UserPrincipal)
-        printfn "%s || %s" user.Name user.DistinguishedName
-
-directActiono()
-
-
-
-
-// This one is getting the right info at least - but is using the old API and gets kinda ugly at the element level
-let directAction() =
-    use searcher = new DirectorySearcher()
-    searcher.Filter = "(&(objectClass=user))" |> ignore
-    let mutable i = 0
-    for sr in searcher.FindAll() do
-        i <- i + 1
-        printfn "%i %O" i sr.Properties.["manager"]
-
-directAction()
-
-
-
-
-
-// Syntax for the findbyident search - not working
-let directActionoo() =
-    use ctx = new PrincipalContext(ContextType.Domain, "ad.hfk.no") //, "OU=HFK");
-    use user = UserPrincipal.FindByIdentity(ctx, "cn=Aaron Winston Comyn")
-    printfn "%s || %s" user.Name user.UserPrincipalName
-
-directActionoo()
-
 
 
 
