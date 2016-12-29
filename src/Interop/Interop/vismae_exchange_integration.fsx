9455f42ab007880113469df253a846467aab1ed4
Aaron Winston Comyn <aaron.winston.comyn@hfk.no>
2016-12-29 11:00:56 +0100

    Working on permissions with Exchange

--- src/Interop/Interop/vismae_exchange_integration.fsx
+++ src/Interop/Interop/vismae_exchange_integration.fsx
@@ -13,7 +13,10 @@ Integration overview
 Integration details
 -------------------
 
-Visma Enterprise is located as "http://hfk-app01:8080/enterprise/enterprise?0"
+The Exchange webservice is located at "https://mail.hfk.no/EWS/Exchange.asmx"
+
+The user account used for this integration requires identity impersonation permission.
+See: https://msdn.microsoft.com/en-us/library/bb204095.aspx
 
 Exchange offers C# wrappers, but they are heavyweight and uninteresting.  Powershell user management against exchange has been used instead...
 
@@ -51,24 +54,56 @@ let webUsers =
 
 //PowerShell.Create().AddScript()
 
-let email, pass, domain = Configuration.ExchangeAdmin.UserName, Configuration.ExchangeAdmin.Password, "mail.hfk.no"
+let email, pass, delegateEmail, domain 
+    = Configuration.ExchangeAdmin.UserName, Configuration.ExchangeAdmin.Password, "vismapost@hfk.no", "mail.hfk.no"
+
+printfn "%s %s %s" email delegateEmail domain
 
 
+
+
+// Working code... Local account, only access to my own mailbox...
+let es = new ExchangeService()
+es.UseDefaultCredentials <- true
+es.Url <- new Uri("https://mail.hfk.no/EWS/Exchange.asmx")
+let emb = new Mailbox("aarcomy@hfk.no") // "anette.ovreas@hfk.no")
+let eds = es.GetDelegates(emb, true)
+let service = es
+
+// Explicit connection, not working...
 let service = new ExchangeService()
-service.Credentials <- new WebCredentials(email, pass, domain)
+service.Credentials <- new WebCredentials(@"adm_aarcomy", "", "hfk")
+service.ImpersonatedUserId <- new ImpersonatedUserId(ConnectingIdType.SmtpAddress, "aarcomy@hfk.no")
+
+
+// use impersonation FROM aarcomy TO adm_aarcomy??
+//service.ImpersonatedUserId <- new ImpersonatedUserId(ConnectingIdType.SmtpAddress, "aarcomy@hfk.no")
+
 service.Url <- new Uri("https://mail.hfk.no/EWS/Exchange.asmx")
-let i : int [] = [|2|]
+let userMailbox = new Mailbox("aarcomy@hfk.no") // "anette.ovreas@hfk.no")
+let delegates = service.GetDelegates(userMailbox, true)
+for d in delegates.DelegateUserResponses do//for d in delegates do
+    printfn "%s" d.DelegateUser.UserId.DisplayName
+
+
 let mailboxQuery = [| new MailboxQuery("Aaron", [|  |]) |]
 let searchParameters = new SearchMailboxesParameters(SearchQueries = mailboxQuery)
 let searchBox = service.SearchMailboxes(searchParameters) |> Seq.head
-let userMailbox = new Mailbox(Configuration.ExchangeAdmin.UserName)
+let userMailbox = new Mailbox("aarcomy@hfk.no")
 let delegates = service.GetDelegates(userMailbox, true)
+service.TraceEnablePrettyPrinting <- true
+service.TraceEnabled <- true
+service.TraceFlags = TraceFlags.All
+
+Environment.UserDomainName
+Environment.UserName
+
 
 let scope  = System.Nullable(MeetingRequestsDeliveryScope.DelegatesAndMe )
 
-let delegateUser = new DelegateUser
+let delegateUser = new DelegateUser(delegateEmail)
 
-service.AddDelegates(userMailbox, scope, )
+service.AddDelegates(userMailbox, scope, delegateUser)
 
 //service.AddDelegates(usermailbox, MeetingRequestsDeliveryScope.DelegatesAndMe, [| delegateuser |])
 
