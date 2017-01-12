84f7d20c7c6edabd5464a3f3422aa22caeef09cf
Aaron Winston Comyn <aaron.winston.comyn@hfk.no>
2017-01-12 11:33:33 +0100

    Serialization of records to create AVRO goodies...

--- src/Interop/Interop/fsharp_avro_generation.fsx
+++ src/Interop/Interop/fsharp_avro_generation.fsx
@@ -1,1 +1,17 @@
 ﻿
+#I "../../packages/"
+#r "Microsoft.Hadoop.Avro/lib/net45/Microsoft.Hadoop.Avro.dll"
+#r "System.Runtime.Serialization.dll"
+
+open System.Runtime.Serialization;
+open System.Data
+open Microsoft.Hadoop.Avro
+open Microsoft.Hadoop.Avro.Container
+
+[<DataContract>]
+type Person = 
+    { Name : string
+      [<DataMember(Name = "Age")>]
+      Age : int }
+
+let serializer = AvroSerializer.Create<Person>()  /// (typeof<Person>)
