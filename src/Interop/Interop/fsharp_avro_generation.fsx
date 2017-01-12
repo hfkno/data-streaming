6cfd953e590f17fd3d0bec80dd545e183628cca2
Aaron Winston Comyn <aaron.winston.comyn@hfk.no>
2017-01-12 15:13:09 +0100

    Record serialization for schema creation

--- src/Interop/Interop/fsharp_avro_generation.fsx
+++ src/Interop/Interop/fsharp_avro_generation.fsx
@@ -2,16 +2,58 @@
 #I "../../packages/"
 #r "Microsoft.Hadoop.Avro/lib/net45/Microsoft.Hadoop.Avro.dll"
 #r "System.Runtime.Serialization.dll"
+#r "Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
+
 
 open System.Runtime.Serialization;
 open System.Data
 open Microsoft.Hadoop.Avro
 open Microsoft.Hadoop.Avro.Container
+open Newtonsoft.Json
+open Newtonsoft.Json.Linq
 
-[<DataContract>]
+
+
+
 type Person = 
     { Name : string
-      [<DataMember(Name = "Age")>]
       Age : int }
 
-let serializer = AvroSerializer.Create<Person>()  /// (typeof<Person>)
+[<DataContract>]
+type PersonC() =
+    let mutable name : string = null
+    let mutable age : int = 0
+    member x.Name
+        with get() = name
+        and set(c) = name <- c
+    [<DataMember(Name = "Age")>]
+    member x.Age
+        with get() = age
+        and set(a) = age <- a
+
+
+let serializer = AvroSerializer.Create<PersonC>()   // only wants to serializepublic getters n such...
+                                                    // I want records...
+printfn "%s" (serializer.WriterSchema.ToString())
+
+
+
+
+
+let json = JsonConvert.SerializeObject({ Name = "OI"; Age = 123 })
+
+let jo = JObject.Parse(json)
+
+for p in jo.Properties() do
+    printfn "%s" p.Name
+    printfn "%A"  (p.Value.ToString())
+
+
+
+
+
+
+
+
+
+
