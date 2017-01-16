620acdfa5aea69c3b3b1716942ee0de6398ba4a2
Aaron Winston Comyn <aaron.winston.comyn@hfk.no>
2017-01-16 13:07:52 +0100

    Utility methods for pub

--- src/Interop/Interop/hip.fsx
+++ src/Interop/Interop/hip.fsx
@@ -6,7 +6,9 @@
 
 open Fsharp_avro_generation
 open Streaming
-
+open System.Linq
+open Newtonsoft.Json
+open Newtonsoft.Json.Linq
 
 // Push a sched to kafka
         // independent sched which pushes jobs when they are read
@@ -49,17 +51,52 @@ r.registerSchema(schema.Name.ToLower()  + "_telemetry-value", schemaJson)
 
 let utcNow = System.DateTime.UtcNow.ToString("s") + "Z" 
 
-let reg = 
+
+let getReg() =
     { Status = "Complete"
       Message = "Registration"
       Created = utcNow }
 
 
+let k = Streams.messageLog()
+
+for i in 1 .. 10000 do
+    printfn "%i" i
+    printf "%A" 
+        (k.publishVersionedMessage("hfk.utility.test.orchestration.JobStatus_telemetry", 6, getReg()))
+        
 
 
 
+type PublicationInfo = 
+    { Name : string
+      SchemaId : int }
+
+let pubInfo<'a> (name:string) (purpose:string) = 
+    let schema = SchemaGenerator.generateSchema<'a> name
+    let schemaJson = sprintf """{"schema": "%s"}""" (schema.Schema.Replace("\"", "\\\""))
+
+    let name = schema.Name.ToLower()  + "_" + purpose
+    let json = r.registerSchema(name + "-value", schemaJson) |> sval
+    let id = JObject.Parse(json).Properties().First().Value.Value<int>()
+    { Name = name; SchemaId = id }
 
 
+type RandomTelemetry = 
+    { Message : string
+      Value : int }      
+    
+let info = pubInfo<RandomTelemetry> "hfk.utility.test.orchestration" "telemetry" 
 
 
+let doTelemFill () =
+    async {
+        for i in 1 .. 10000 do
+            printfn "%i" i
+            printfn "%A" 
+                (k.publishVersionedMessage(info.Name, info.SchemaId, {Message="I can publish!!";Value=i}))
+        }
 
+[ doTelemFill(); doTelemFill(); doTelemFill(); doTelemFill()]
+|> Async.Parallel
+|> Async.RunSynchronously
